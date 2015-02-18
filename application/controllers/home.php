e6c4db4af58efe82f6fb53ffcfddc38b90684bf2
Ganesh Bhosale <gdb.sci123@gmail.com>
2015-02-18 17:34:21 +0530

    basic companies loaded

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -6,6 +6,7 @@ class Home extends CI_Controller {
     var $base_path;
     var $stock_files;
     var $data;
+    var $curl_error_str;
     
     public function __construct() {
         parent::__construct();
@@ -15,30 +16,77 @@ class Home extends CI_Controller {
 		$this->stock_files = $this->config->item("stock_files");
         $this->data = array();
         $this->data['base_url'] = $this->base_url;
+        
+        $this->load->library('csvreader');
     }
     
     public function index() {
+		
 		$this->load->view('index', $this->data);
         
         //echo getcwd();
 	}
     
     public function update() {
+        $this->data['error'] = "";
         
         $query = $this->db->get('companies');
 		if($query->num_rows() > 0) {
-            $this->data['message'] = "Companies presents";
+            $this->data['message'] = "Companies presents"."\n<br>";
         } else {
-            $this->data['message'] = "Companies not presents";
-            $filename = date("");
-            $path = "http://www.nseindia.com/content/historical/EQUITIES/2015/FEB/cm16FEB2015bhav.csv.zip";
+            $this->data['message'] = "Companies not presents"."\n<br>";
             
-            $this->data['message'] = $path;
+            // download last day file
+            $daypast = 2;
+            $filename = "";
+            $isFileReceived = false;
+            while(!$isFileReceived) {
+                $day = date('d',strtotime("-".$daypast." days"));
+                $month_str = strtoupper(date('M',strtotime("-".$daypast." days")));
+                $year = date('Y',strtotime("-".$daypast." days"));
+
+                $filename = "cm".$day.$month_str.$year."bhav.csv";
+                $path = "http://www.nseindia.com/content/historical/EQUITIES/".$year."/".$month_str."/".$filename.".zip";
+
+                $zipResource = fopen($this->base_path . "/TodaysFile.zip", "w");
+                
+                $page = $this->downloadFile($path, $zipResource);
+                
+                if(!$page) {
+                    $this->data['error'] .= "File: ".$path." : ".$this->curl_error_str."\n<br>";
+                    log_message("error", "File failed: ".$path." : ".$this->curl_error_str);
+                    $daypast += 1;
+                    if($daypast > 3) {
+                        break;
+                    }
+                    $isFileReceived = false;
+                } else {
+                    $this->data['message'] .= "Got file: ".$path."\n<br>";
+                    log_message("debug", "Got file: ".$path." : ".$this->curl_error_str);
+                    $isFileReceived = true;
+                }
+            }
             
+            $this->unzipFile($this->base_path . "/TodaysFile.zip", $this->base_path."/zipFolder");
+            
+            $this->data['message'] .= "Extraction complete"."\n<br>";
+            
+            $csvData = $this->csvreader->parse_file($this->base_path."/zipFolder/".$filename);
+            
+            foreach($csvData as $field) {
+                //$this->data['message'] .= "".$field['SYMBOL']."<br>";
+                if($field['SERIES'] == "EQ") {
+                    $da = array(
+                        'symbol' => $field['SYMBOL'],
+                        'isin' => $field['ISIN']
+                    );
+                    $this->db->insert('companies', $da);
+                }
+            }
+            $this->data['message'] .= "Got total ".($this->db->insert_id())." companies...\n<br>";
             
             /*
             // download latest files
-            file_put_contents($this->base_path . "Tmpfile.zip", fopen("http://someurl/file.zip", 'r'));
             
             // Open the Zip file
             $zip = new ZipArchive;
@@ -64,4 +112,42 @@ class Home extends CI_Controller {
 
         $this->load->view('update', $this->data);
 	}
+    
+    function unzipFile($zipFile, $folder) {
+        $zip = new ZipArchive;
+            if($zip->open($zipFile) != "true") {
+                $this->data['error'] .= "Error :- Unable to open the Zip File: ".$zipFile."\n<br>";
+                log_message("error", "Unable to open the Zip File: ".$zipFile);
+            }
+            //Empty zipFolder
+            $files = glob($folder."/*");
+            foreach($files as $file) {
+                if(is_file($file))
+                    unlink($file);
+            }
+            // delete zip file
+            unlink($zipFile);
+            // Extract Zip File
+            $zip->extractTo($folder);
+            $zip->close();
+    }
+    
+    function downloadFile($src, $dest) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $src);
+        curl_setopt($ch, CURLOPT_FAILONERROR, true);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+        curl_setopt($ch, CURLOPT_BINARYTRANSFER,true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); 
+        curl_setopt($ch, CURLOPT_FILE, $dest);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13');
+        $page = curl_exec($ch);
+        $this->curl_error_str = curl_error($ch);
+        curl_close($ch);
+        return $page;
+    }
 }
--- application/views/update.php
+++ application/views/update.php
@@ -4,6 +4,9 @@ include("header.php");
 	<div id="body">
         <h5><?php echo $message; ?></h5>
         Updating...
+        <p style="color:red">
+            <?php if(isset($error)) echo $error; ?>
+        </p>
     </div>
 <?php
 include("footer.php");
